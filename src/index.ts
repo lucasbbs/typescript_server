89f39f77321a9d5c9574e91ffe71939cc693435e
Lucas Breno de Souza Noronha Braga <lucasbbs@live.fr>
2022-08-28 19:08:55 -0300

    226 - A Closer Integration

--- src/index.ts
+++ src/index.ts
@@ -2,10 +2,25 @@ import express, { Request, Response } from 'express';
 import loginRoutes from './routes/login.routes';
 import cookieSession from 'cookie-session';
 
-const app = express();
+// app.use(express.urlencoded({ extended: true }));
+// app.use(cookieSession({ keys: ['laskjdf'] }));
+// app.use(loginRoutes);
 
-app.use(express.urlencoded({ extended: true }));
-app.use(cookieSession({ keys: ['laskjdf'] }));
-app.use(loginRoutes);
+// app.listen(3000, () => console.log('Running on port 3000'));
 
-app.listen(3000, () => console.log('Running on port 3000'));
+// DO NOT WRITE THAT
+
+class Server {
+  app: express.Express = express();
+  constructor() {
+    this.app.use(express.urlencoded({ extended: true }));
+    this.app.use(cookieSession({ keys: ['laskjdf'] }));
+    this.app.use(loginRoutes);
+  }
+
+  start(): void {
+    this.app.listen(3000, () => console.log('Running on port 3000'));
+  }
+}
+
+new Server().start();

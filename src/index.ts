2d702510bcc0c8be1ec9d4b6b4e17c6248795ac1
Lucas Breno de Souza Noronha Braga <lucasbbs@live.fr>
2022-08-28 18:40:52 -0300

    223 - Checking Login Status

--- src/index.ts
+++ src/index.ts
@@ -7,12 +7,5 @@ const app = express();
 app.use(express.urlencoded({ extended: true }));
 app.use(cookieSession({ keys: ['laskjdf'] }));
 app.use(loginRoutes);
-app.get('/', (req: Request, res: Response) => {
-  res.send(`
-  <div>  
-    <h1>Hi, there!</h1>
-  </div>
-  `);
-});
 
 app.listen(3000, () => console.log('Running on port 3000'));

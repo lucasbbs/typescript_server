f83763c98b7eec66a7b261f116bd52722cab5278
Lucas Breno de Souza Noronha Braga <lucasbbs@live.fr>
2022-08-29 11:14:35 -0300

    238 - Project Overview

--- src/routes/login.routes.ts
+++ src/routes/login.routes.ts
@@ -71,3 +71,28 @@ router.get('/protected', requireAuth, (req: Request, res: Response) => {
 });
 
 export default router;
+
+// DO NOT WRITE
+/*
+@controller('/auth')
+class LoginController {
+  @get('/login')
+  getLogin(req: Request, res: Response): void {
+    res.send('form');
+  }
+
+  @post('/login')
+  @validateBody('email', 'password')
+  @use(requireAuth)
+  postLogin(req: Request, res: Response): void {
+    const { email, password } = req.body;
+
+    if (email && password && email === 'hi@hi.com' && password === 'password') {
+      req.session = { loggedIn: true };
+      res.redirect('/');
+    } else {
+      res.send('Invalid Email or Password');
+    }
+  }
+}
+*/

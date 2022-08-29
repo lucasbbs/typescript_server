59b06aaeee8fd869e69f0510e9c48a63c489610f
Lucas Breno de Souza Noronha Braga <lucasbbs@live.fr>
2022-08-29 13:15:19 -0300

    248 - A Few Fixups

--- src/controllers/decorators/controller.ts
+++ src/controllers/decorators/controller.ts
@@ -1,19 +1,15 @@
 import 'reflect-metadata';
-import { Router } from 'express';
-
-const router = Router();
+import { AppRouter } from '../../app.routes';
 
 export function controller(routePrefix: string) {
   return function (target: Function) {
+    const router = AppRouter.getInstance();
     for (const key in target.prototype) {
       const routeHandler = target.prototype[key];
       const path = Reflect.getMetadata('path', target.prototype, key);
-
       if (path) {
         router.get(`${routePrefix}${path}`, routeHandler);
       }
     }
   };
 }
-
-export { router };
--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,7 @@
 import express, { Request, Response } from 'express';
 import loginRoutes from './routes/login.routes';
 import cookieSession from 'cookie-session';
-import { router as controllerRouter } from './controllers/decorators/controller';
+import { AppRouter } from './app.routes';
 
 import './controllers/LoginController';
 
@@ -9,7 +9,7 @@ const app = express();
 app.use(express.urlencoded({ extended: true }));
 app.use(cookieSession({ keys: ['session'] }));
 app.use(loginRoutes);
-app.use(controllerRouter);
+app.use(AppRouter.getInstance());
 
 app.listen(3000, () => {
   console.log('Running on port 3000');

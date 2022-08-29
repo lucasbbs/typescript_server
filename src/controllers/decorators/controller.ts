9d33f35533d604ec2cb708245841add993219ace
Lucas Breno de Souza Noronha Braga <lucasbbs@live.fr>
2022-08-29 13:52:02 -0300

    252 - The 'Use' Decorator

--- src/controllers/decorators/controller.ts
+++ src/controllers/decorators/controller.ts
@@ -13,13 +13,16 @@ export function controller(routePrefix: string) {
         target.prototype,
         key
       );
-      const method: Methods = Reflect.getMetadata(
-        MetadataKeys.method,
-        target.prototype,
+      const method: Methods =
+        Reflect.getMetadata(MetadataKeys.method, target.prototype, key) || [];
+
+      const middlewares = Reflect.getMetadata(
+        MetadataKeys.middleware,
+        target,
         key
       );
       if (path) {
-        router[method](`${routePrefix}${path}`, routeHandler);
+        router[method](`${routePrefix}${path}`, ...middlewares, routeHandler);
       }
     }
   };
